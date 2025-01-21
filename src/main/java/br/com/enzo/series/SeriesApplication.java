7585394e6f657aa7709986cbb80c8639b30e656d
Enzo Calasans <Enzocalasanscardosocorreacosta@gmail.com>
2025-01-21 20:05:34 -0300

    Segundo commit

---
+++ src/main/java/br/com/enzo/series/SeriesApplication.java
@@ -0,0 +1,23 @@
+package br.com.enzo.series;
+
+import br.com.enzo.series.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SeriesApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeriesApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+
+	}
+}
